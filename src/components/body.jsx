7b792518d5b10c177f250cefc8e630cf4af029c1
rm5973 <masurnaikrakshit@gmail.com>
2022-08-16 12:56:46 +0530

    comments

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -2,7 +2,7 @@ function Body(){
     return(
         <div className="bg-gokart-image w-full h-[50vw] bg-cover bg-centre items-center	px-4 flex items-center	">
             <div>
-            <h1 class="text-[5.625vw] text-neutral-50 ">AutoCreed</h1>
+             <h1 class="text-[5.625vw] text-neutral-50 ">AutoCreed</h1> {/*vw for responsive website */}
             <h2 class="text-[3.75vw] text-neutral-50 text-left">Racing</h2>
             <h3 class="text-[2.8125vw] text-neutral-50 text-left">To be a team above all and above all a team.</h3>
             </div>
