1f043339350a6fd427ab5109f0006362d6f3e0bb
rm5973 <masurnaikrakshit@gmail.com>
2022-08-16 12:55:17 +0530

    responsive editing

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,10 +1,10 @@
 function Body(){
     return(
-        <div className="bg-gokart-image w-full h-screen bg-cover bg-centre items-center	px-4 flex items-center	">
+        <div className="bg-gokart-image w-full h-[50vw] bg-cover bg-centre items-center	px-4 flex items-center	">
             <div>
-            <h1 class="text-7xl text-neutral-50 ">AutoCreed</h1>
-            <h2 class="text-5xl text-neutral-50 text-left">Racing</h2>
-            <h3 class="text-3xl text-neutral-50 text-left">To be a team above all and above all a team.</h3>
+            <h1 class="text-[5.625vw] text-neutral-50 ">AutoCreed</h1>
+            <h2 class="text-[3.75vw] text-neutral-50 text-left">Racing</h2>
+            <h3 class="text-[2.8125vw] text-neutral-50 text-left">To be a team above all and above all a team.</h3>
             </div>
         </div>
     )
