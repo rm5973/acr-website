1f860a89839d42a46ec0e16f4c673ef4e80313a2
Prasoonsahay2003 <pa6992@srmist.edu.in>
2022-08-19 08:19:44 +0530

    line 10,12 today gmeet mei dekhte hai

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -6,6 +6,29 @@ function Body(){
             <h2 class="text-5xl text-neutral-50 text-left">Racing</h2>
             <h3 class="text-3xl text-neutral-50 text-left">To be a team above all and above all a team.</h3>
             </div>
+            
+            <div class="bg-red-400 h-full"></div> {/*second page*/}
+            <div class="h-full bg-red-500">
+            <div class="grid grid-cols-1 divide-y divide-yellow-00">
+ 
+            <div class="shadow-xl border-t-2 border-white-500"></div>
+            <div class ="shadow-xl border-t-2 border-white-500 text-white
+  text-gray-200 text-4xl p-4">what does acr do?</div>
+             <div class="shadow-xl border-t-2 border-white-50"></div>
+</div>
+</div>
+<div class="h-full bg-slate-800">black</div>{/*third page*/}
+/*body*/ <nav>
+<ul className='hidden md:flex gap-14 p- uppercase bg-white/10 '>
+            <li>Home</li>
+            <li>About</li>
+            <li>Departments</li>
+            <li>Team</li>
+            <li>Achievements</li>
+            
+        </ul>
+        </nav>
+
         </div>
     )
 }
